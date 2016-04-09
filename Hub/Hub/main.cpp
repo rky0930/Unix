ff5fc42aa2612b6a2d3f7b0935dd48874ce0b539
YOOGYOUNGYOON <rky0930@gmail.com>
2016-04-09 10:54:37 +0900

    Hub project first commit.

---
+++ Hub/Hub/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Hub
+//
+//  Created by YOOGYOUNGYOON on 2016. 4. 9..
+//  Copyright © 2016년 YOOGYOUNGYOON. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
